fd604bb742b28898192d8f0164f4a6da5e0997df
Yıldız Mamak <yldzmamak@gmail.com>
2018-12-31 14:25:33 +0300

    navigationDrawer added

--- app/src/main/java/com/example/yldzufk/genclerburada/LoginAfterActivity.java
+++ app/src/main/java/com/example/yldzufk/genclerburada/LoginAfterActivity.java
@@ -1,16 +1,39 @@
 package com.example.yldzufk.genclerburada;
 
+import android.media.Image;
+import android.os.Build;
+import android.support.annotation.NonNull;
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.yldzufk.genclerburada.ui.common.NavigationController;
 
-public class LoginAfterActivity extends AppCompatActivity {
+import static android.view.Gravity.LEFT;
+import static android.view.Gravity.RIGHT;
 
+public class LoginAfterActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         NavigationController navigationController = new NavigationController(this);
 
@@ -18,5 +41,39 @@ public class LoginAfterActivity extends AppCompatActivity {
             navigationController.navigateToConfessionList();
         }
 
+        drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
+        drawer.closeDrawer(GravityCompat.START);
+
+
+
+
+        return true;
+    }
+
+
+
+
+
 }
